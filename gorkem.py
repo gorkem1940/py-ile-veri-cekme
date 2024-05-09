4259cb227622247fd9ef8c937535734151280cbf
gorkem <113816295+gorkem1940@users.noreply.github.com>
2024-05-09 09:26:11 +0300

    Add files via upload

---
+++ gorkem.py
@@ -0,0 +1,29 @@
+import requests
+import xml.etree.ElementTree as ET
+import time 
+
+def get_external_xml(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+    
+def write_to_xml(data, filename):
+    root = ET.fromstring(data)
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+if __name__ == "__main__":
+    external_url = "http://88.250.204.138:4545/status.xml"
+    local_xml_file = "status.xml"
+
+    while True:
+        external_data = get_external_xml(external_url)
+        if external_data:
+            write_to_xml(external_data, local_xml_file)
+        else:
+            print("Dış ıp üzerinden veri alınamadı")
+
+        time.sleep(30)
+ 
